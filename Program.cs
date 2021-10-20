3695957ccd14a93c81c002db4b6faee80926bfb8
Galya <58034004+Galya777@users.noreply.github.com>
2021-10-21 02:09:35 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace week1task5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number= Console.Read();
+            for(int i = 0; i < number; ++i)
+            {
+                int N = (int)(i * Math.Pow(4, i));
+                switch (N)
+                {
+                    case 0: Console.Write("A");
+                        break;
+                    case 1:
+                        Console.Write("C");
+                        break;
+                    case 2:
+                        Console.Write("G");
+                        break;
+                    case 3:
+                        Console.Write("T");
+                        break;
+                }
+            }
+            
+        }
+    }
+}

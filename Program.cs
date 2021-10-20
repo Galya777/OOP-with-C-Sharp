1674deb0364997eaa4ab579c1562b82d9fa7ff4b
Galya <58034004+Galya777@users.noreply.github.com>
2021-10-21 01:46:38 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace task2week1
+{
+    class Program
+    {
+        public static int FindMedian(int number1, int number2, int number3)
+        {
+            if (number1 > number3)
+            {
+                return Math.Max(Math.Min(number1, number2), number3);
+            }
+            else
+            {
+                return Math.Min(Math.Max(number1, number2), number3);
+            }
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine(FindMedian(30, 20, 10));
+        }
+    }
+}

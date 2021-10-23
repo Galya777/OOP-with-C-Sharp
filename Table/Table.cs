9507229125e3cc3ef90bd0c2d63781950db5f05b
Galya <58034004+Galya777@users.noreply.github.com>
2021-10-23 12:16:57 +0300

    Add files via upload

---
+++ Table/Table.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework1_45616
+{public class TableDiagram
+    {
+        class Table
+        {
+            private double a;
+            private double b;
+            private int step;
+            public double A { get => a; set => a = value; }
+
+            public double B { get => b; set => b = value; }
+            public int Step { get => step; set => step = value > 0 ? value : 1; }
+
+            public Table(double a, double b, int step)
+            {
+                this.a = a;
+                this.b = b;
+                this.Step = step;
+
+            }
+            public void MakeTable()
+            {
+                int count = 0;
+                double x = a;
+                double size = (b - a) / step;
+                Console.WriteLine("x\t\tF(x)");
+                do
+                {
+                    Console.WriteLine("%.4f\t%.4f\n", x, (x - 2) * (x - 2) / (x * x + 1));
+                    ++count;
+                    if (count % 20 == 0)
+                    {
+                        Console.WriteLine("Press Return to continue...");
+                        Console.ReadKey();
+                    }
+                    x += size;
+                } while (x <= b);
+            }
+
+
+        }
+    }
+}
---
+++ Table/TableTest.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework1_45616
+{
+    class TableTest
+    {
+        public static void main()
+        {
+            double a, b;
+            int step;
+            Console.WriteLine("Enter the left endpoint of interval:");
+            string aText = Console.ReadLine();
+            a = Double.Parse(aText);
+            Console.WriteLine("Enter the right endpoint of interval:");
+            string bText = Console.ReadLine();
+            b = Double.Parse(bText);
+            Console.WriteLine("Enter the left endpoint of interval:");
+            string stepText = Console.ReadLine();
+            step = Int32.Parse(stepText);
+            if (a > b)
+            {
+                double temp = a;
+                a = b;
+                b = temp;
+            }
+                Table table = new Table(a, b, step);
+                table.MakeTable();
+            
+        }
+    }
+}

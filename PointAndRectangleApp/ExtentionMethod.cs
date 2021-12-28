6b94b4ef146d990970cf6784ab88a0e7e5b94630
Galya <58034004+Galya777@users.noreply.github.com>
2021-12-28 18:23:13 +0200

    Add files via upload

---
+++ PointAndRectangleApp/ExtentionMethod.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Week7_Problem2Test
+{
+   public class ExtentionMethod
+    {
+        public static double Perimeter(this Rectangle rectangle)
+        {
+            var lpCoordinates = rectangle.LowerRightPoint.Coordinates;
+            var upCoord= rectangle.UpperRightPoint.Coordinates;
+            var length= Math.Abs(rectangle.LowerRightPoint[0]- rectangle.UpperRightPoint[0]);
+            var width= Math.Abs(rectangle.LowerRightPoint[1] - rectangle.UpperRightPoint[1]);
+            return 2 * (length + width);
+        }
+    }
+}
---
+++ PointAndRectangleApp/Rectangle.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace week7_task2
+{
+   public class Rectangle:Point
+    {
+
+        private Point lowerRightPoint;
+        
+
+        #region Constructors
+        //General-Purpose
+        public Rectangle(Point UpperRightPoint, Point lowerRightPoint):base(UpperRightPoint)
+        {
+            LowerRigthPoint = lowerRightPoint;
+        }
+        //Default 
+        public Rectangle():this(new Point(),new Point())
+        {
+
+        }
+        //Copy
+        public Rectangle(Rectangle r):this(r.UpperRightPoint, r.LowerRigthPoint)
+        {
+                
+        }
+        #endregion
+        public Point LowerRigthPoint //Point is reference nutable type, has set Property
+        {
+            get { return new Point(lowerRightPoint); }
+            set { lowerRightPoint = value!=null?new Point(value):new Point(); }
+        }
+
+        public Point UpperRightPoint //Point is reference nutable type, has set Property
+        {
+            get
+            {
+                return new Point(Coordinates);
+            }
+            set
+            {
+                Coordinates = value != null ? value.Coordinates : new int[2];
+            }
+        }
+
+        public override string ToString()
+        {
+            return string.Format($"R: L={base.ToString()}, R={lowerRightPoint}");
+        }
+
+    }
+}

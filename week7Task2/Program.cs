094e495618768d1469cc9e1a3d0d8662135e278d
Galya <58034004+Galya777@users.noreply.github.com>
2022-11-26 17:42:26 +0200

    Add files via upload

---
+++ week7Task2/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace week7_task2
+{
+    class Program
+    {
+        /*
+        static void main(string[] args)
+        {
+            point point = new point();
+            console.writeline(point);
+            point.coordinates = new int[] { 10, 10 };
+            console.writeline(point);
+            point point2 = new point(new int[] { 100, 200 });
+            rectangle rectangle = new rectangle(point, point2);
+            console.writeline(rectangle);
+            point2.coordinates = new int[2];
+            console.writeline(rectangle);
+        }*/
+    }
+}
---
+++ week7Task2/ProgramTest.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Week7_Problem2Test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Point point = new Point();
+        }
+    }
+}
